c1ea66f9ee56ff732dce44c0d8558c67175ba73b
Sameer Mirza <israfil334@gmail.com>
2021-06-02 13:21:30 -0700

    added build args

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,11 @@
 IMAGE_NAME="$INPUT_GCR_HOST/$INPUT_GCR_PROJECT_ID/$INPUT_DOCKER_IMAGE_NAME:$INPUT_DOCKER_IMAGE_TAG"
 
 echo $INPUT_GCR_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
-docker build -t $IMAGE_NAME -f $INPUT_DOCKERFILE $INPUT_DOCKER_CONTEXT
+if [ -z "$INPUT_BUILD_ARGS" ]
+then
+      docker build -t $IMAGE_NAME -f $INPUT_DOCKERFILE $INPUT_DOCKER_CONTEXT
+else
+      docker build -t $IMAGE_NAME -f --build-arg $INPUT_BUILD_ARGS $INPUT_DOCKERFILE $INPUT_DOCKER_CONTEXT
+fi
+
 docker push $IMAGE_NAME
